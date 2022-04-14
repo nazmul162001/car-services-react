1d5b59fff86169123c75c40d3b18d9d12ba87229
nazmul162001 <nazmulprime162001@gmail.com>
2022-04-14 11:47:11 +0600

    get data from email & password using useRef

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import About from './Pages/About/About';
 import Home from './Pages/Home/Home/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ServiceDetail from './Pages/ServiceDetail/ServiceDetail';
+import NotFound from './Shared/NotFound/NotFound';
+import Login from './Pages/Login/Login/Login';
+import Register from './Pages/Login/Register/Register';
 
 function App() {
   return (
@@ -15,6 +18,9 @@ function App() {
         <Route path='/home' element={<Home></Home>}></Route>
         <Route path='/service/:serviceId' element={<ServiceDetail></ServiceDetail>}></Route>
         <Route path='about' element={<About></About>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/register' element={<Register></Register>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
       <Footer></Footer>
     </>
--- src/Shared/NotFound/NotFound.js
+++ src/Shared/NotFound/NotFound.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import notFound from  '../../images/404.png'
 const NotFound = () => {
   return (
     <div>
-      
+      <img style={{width:'100%', height: '100vh'}} src= {notFound} alt="" />
     </div>
   );
 };
